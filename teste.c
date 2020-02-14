be6aae4a92995dce27b27c47764afde0efce5309
aluno <anthonysheridanbarbaro@gmail.com>
2020-02-14 10:11:46 -0300

    sei verificar par ou impar

--- teste.c
+++ teste.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdio_ext.h>
 
 int main(){
 
+    int a;
+    char x;
+
+    system("clear");
+    do {
+
+    printf("\n\tDigite um numero inteiro: ");
+    scanf("%i", &a);
+
+    if(a%2==0){
+        printf("\n\tO numero '%i' e um numero par\n", a);
+    }
+    else printf("\n\tO numero '%i' e um numero impar\n", a);
+
+    printf("\n\tDeseja executar novamente? ");
+    __fpurge(stdin);
+    scanf("%c", &x);
+
+    }while(x=='S' || x=='s');
 
     return 0;
 }
